25b96800898da07c3600ee029cf750d4bae5ed66
Metelger <43734213+Metelger@users.noreply.github.com>
2019-03-01 22:08:09 -0300

    Add files via upload
    Desafio para saber qual combustivel é mais rentável.

---
+++ DesafioCombustivel.java
@@ -0,0 +1,15 @@
+public class DesafioCombustivel {
+	public static void main(String[] args) {
+		//var: gasolina, alcool
+		//Se gasolina * 0.7 < que o valor do alcool, gasolina compensa
+		double gasolina = 3.9;
+		double alcool = 2.8;
+		
+		if (gasolina * 0.7 < alcool) {
+			System.out.println("Compensa abastecer com gasolina!");
+		}else {
+			System.out.println("Compensa abastecer com alcool!");
+		}
+	}
+
+}
